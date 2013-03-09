85d79a2687adb24bf1a8b2362e03915c1ae2462d
MoyCaKu <moises.cauich@gmail.com>
2013-03-08 21:25:42 -0600

    Nuevo archivo

---
+++ EvolucionaMovil/Repositories/ProspectosRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EvolucionaMovil.Models;
+using cabinet.patterns.clases;
+
+namespace EvolucionaMovil.Repositories
+{
+    public class ProspectosRepository : RepositoryBase<Prospecto,EvolucionaMovilBDEntities>
+    {
+        public bool ExisteEmail(string Email)
+        {
+            return context.Prospectos.Any(p => p.Email == Email);
+        }
+    }
+}
