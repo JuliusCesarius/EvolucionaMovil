cfc99f9c4d9580c329d5df463e18fc2bb02b45a0
JuliuCesarius <michavo2000@gmail.com>
2013-03-25 23:37:53 -0600

    Cambios PayCenter Moy

---
+++ EvolucionaMovil/Models/BR/PaycenterBR.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using EvolucionaMovil.Repositories;
+
+namespace EvolucionaMovil.Models.BR
+{
+    public class PaycenterBR : cabinet.patterns.clases.BusinessRulesBase<PayCenter>
+    {
+        public static ValidationResult UsuarioUnico(string value)
+        {
+            PayCentersRepository repository = new PayCentersRepository();
+            bool Existe = repository.ExisteUsuario(value);
+            if (Existe)
+            {
+                return new ValidationResult("El usuario ya existe en la base de datos.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
