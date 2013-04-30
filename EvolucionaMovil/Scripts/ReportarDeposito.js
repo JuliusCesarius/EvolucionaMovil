3a9ffd71586fdfac0ffb3b61a83b6c7209796547
joselara2383 <aries2383@hotmail.com>
2013-04-29 21:29:56 -0500

    modificacion de los JS y Se agrego el combo para obtener las cuentas de deposito.

--- EvolucionaMovil/Scripts/ReportarDeposito.js
+++ EvolucionaMovil/Scripts/ReportarDeposito.js
@@ -1,16 +1,34 @@
 ﻿$(document).on("ready", function () {
     $("#Fecha").datepicker({ "dateFormat": "dd/mm/yy", maxDate: '0' });
     $("#btnSave").on("click", Save);
+      $("#CuentaBancariaId").on("change", function () { $("#hddCuentaBancaria").val($("#CuentaBancariaId").find(":selected").text()); });
+  
+    // $("#CuentaId").on("change", function () { $("#hddTipoCuenta").val($("#CuentaId").find(":selected").text()); });
+      $("#CuentaId").on("change", CuentaDeposito);
     $('#BancoId').on("change", BancoIdChanged);
     if ($("#BancoId").val() > 0) {
         $('#BancoId').trigger("change");
-        $("#CuentaId").val($('#hddCuentaId').val());
+        $("#CuentaBancariaId").val($('#hddCuentaBancariaId').val());
     }
+
     $(".cuentadeposito").each(function (i, item) {
         $("form").validate().settings.rules[$(item).find("input[type=text]").attr("name")] = { "number": true };
     });
     $(".cuentadeposito input[type=text]").on("blur", CuentasDepositoChanged);
 });
+
+//$("#Rechazar").css("display", "inline");
+
+function CuentaDeposito() {
+    $("#hddTipoCuenta").val($("#CuentaId").find(":selected").text());
+    var cuentaDepositoId = $("#CuentaId").val();
+    if (cuentaDepositoId != null && cuentaDepositoId > 0) {
+        $("#Imagen").css("display", "inline");
+    }
+    else {
+        $("#Imagen").css("display", "none");
+    }
+}
 function CuentasDepositoChanged(event) {
     var monto = parseFloat($("#Monto").val());
     if (isNaN(monto)) {
@@ -50,18 +68,35 @@ function Save(event) {
     }
 }
 function BancoIdChanged() {
-        var bancoId = $(this).val();
-        if (bancoId != null) {
-            var CuentaId = $("#CuentaId");
-            CuentaId.html("");
+    var bancoId = $(this).val();
+    $("#hddBanco").val($("#BancoId").find(":selected").text());
+    if (bancoId != null) {
+        var CuentaBancariaId = $("#CuentaBancariaId");
+        CuentaBancariaId.html("");
+        if (bancoId == 0) {
+            CuentaBancariaId.html("<option/>");
+            CuentaBancariaId.val(0);
+        } else {
+        var cdefault = 0;
             var cuentas = $.parseJSON($("#hddCuentas").val());
             $.each(cuentas, function (i, item) {
                 if (item.BancoId == bancoId) {
-                    CuentaId.append($('<option/>', {
-                        value: item.CuentaId,
+                    CuentaBancariaId.append($('<option/>', {
+                        value: item.CuentaBancariaId,
                         text: item.NumeroCuenta + " - " + item.Titular
                     }));
                 }
+                if (i == 0) { cdefault = item.CuentaBancariaId; }
             });
+
+
+            if (cdefault > 0) {
+                $("#CuentaBancariaId option[value=" + cdefault + "]").attr("selected", true);
+                $("#hddCuentaBancaria").val($("#CuentaBancariaId").find(":selected").text());
+            }
+
+
         }
+    }
 }
+
