b66207e4296df9777addf99545e86ab2f9bfa415
JuliuCesarius <michavo2000@gmail.com>
2013-05-10 01:13:05 -0500

    Fix Deploymet Configuration: * Se agregó las instrucciones transform al web.config del release * Se modificaron la configuración del deployment * Falta checar si funciona correctamente el Global.asax con los Unhandled errors

--- EvolucionaMovil/Global.asax.cs
+++ EvolucionaMovil/Global.asax.cs
@@ -32,32 +32,39 @@ namespace EvolucionaMovil
 
         protected void Application_Error()
         {
-            var exception = Server.GetLastError();
-            var httpException = exception as HttpException;
-            Response.Clear();
-            Server.ClearError();
-            var routeData = new RouteData();
-            routeData.Values["controller"] = "Error";
-            routeData.Values["action"] = "General";
-            routeData.Values["exception"] = exception;
-            Response.StatusCode = 500;
-            if (httpException != null)
+            try
             {
-                Response.StatusCode = httpException.GetHttpCode();
-                switch (Response.StatusCode)
+                var exception = Server.GetLastError();
+                var httpException = exception as HttpException;
+                if (httpException != null)
                 {
-                    //case 403:
-                    //    routeData.Values["action"] = "Http403";
-                    //    break;
-                    case 404:
-                        routeData.Values["action"] = "NotFound";
-                        break;
+                    Response.Clear();
+                    Server.ClearError();
+                    var routeData = new RouteData();
+                    routeData.Values["controller"] = "Error";
+                    routeData.Values["action"] = "General";
+                    routeData.Values["exception"] = exception;
+                    Response.StatusCode = 500;
+
+                    Response.StatusCode = httpException.GetHttpCode();
+                    switch (Response.StatusCode)
+                    {
+                        //case 403:
+                        //    routeData.Values["action"] = "Http403";
+                        //    break;
+                        case 404:
+                            routeData.Values["action"] = "NotFound";
+                            break;
+                    }
+
+                    IController errorsController = new ErrorController();
+                    var rc = new RequestContext(new HttpContextWrapper(Context), routeData);
+                    errorsController.Execute(rc);
                 }
             }
-
-            IController errorsController = new ErrorController();
-            var rc = new RequestContext(new HttpContextWrapper(Context), routeData);
-            errorsController.Execute(rc);
+            catch (Exception ex)
+            {
+            }
         }
 
         protected void Application_Start()
